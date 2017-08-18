88b8165133781adf505bc0524ab695a1015593e3
Kyle Treptow <kyletreptow@Kyles-MBP-2.fios-router.home>
2017-08-17 20:14:10 -0700

    removing/editing express views

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var access_log_controller = require('../controllers/access-log');
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Home' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
 
 /* GET mongo-db page. */
 // router.get('/mongo-db', access_log_controller.display_log);
