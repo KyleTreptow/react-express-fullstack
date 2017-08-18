754abc85c61ce2b67c4398c0fe9b55dab543b4a4
Kyle Treptow <kyletreptow@Kyles-MBP-2.fios-router.home>
2017-08-17 20:09:13 -0700

    changing the mlab account, new collection connected

--- controllers/access-log.js
+++ controllers/access-log.js
@@ -6,47 +6,16 @@ mongoose.Promise = global.Promise;
 // try to redeclare a model.
 var storedModels = {};
 
-/////////////////////////////
-// Display Log
-/////////////////////////////
-exports.display_log = function(req, res, next) {
-  var mongoHost = 'apollo:interactive@ds129143.mlab.com:29143/apollo_demo';
-  var promise = mongoose.connect(
-    'mongodb://' + mongoHost,
-    { useMongoClient: true}
-  );
-
-  promise.then(function(db) {
-    // Use the stored schema if possible, otherwise create it
-    if(storedModels['access_log']) {
-      var access = storedModels['access_log'];
-    } else {
-     // Usually this is stored in a different folder, but for now put it here
-     var access = storedModels['access_log'] = db.model('access_log', { date: String });
-    }
-
-    var newAccess = new access;
-    newAccess.date = new Date();
-
-    newAccess.save(function(err) {
-      if(err) {
-        res.send("Error saving into database");
-      } else {
-        access.find(function(err, log) {
-          res.render('mongo-db', {title: "MongoDB Log Page", log: log.reverse(), host: mongoHost});
-        })
-      }
-    })
-
-  })
-}
+var userName = 'kyletreptow';
+var userPass = 'Spitfire7&';
+var userDB = 'ds149373.mlab.com:49373/treptow-express';
+var mongoHost = userName+':'+userPass+'@'+userDB;
 
 
 /////////////////////////////
 // Provide Endpoint
 /////////////////////////////
 exports.provide_endpoint = function(req, res, next) {
-  var mongoHost = 'apollo:interactive@ds129143.mlab.com:29143/apollo_demo';
   var promise = mongoose.connect(
     'mongodb://' + mongoHost,
     { useMongoClient: true}
@@ -71,6 +40,42 @@ exports.provide_endpoint = function(req, res, next) {
       } else {
         access.find(function(err, log) {
           res.send(JSON.stringify(log.reverse()));
+        })
+      }
+    })
+
+  })
+}
+
+
+
+/////////////////////////////
+// Display Log
+/////////////////////////////
+exports.display_log = function(req, res, next) {
+  var promise = mongoose.connect(
+    'mongodb://' + mongoHost,
+    { useMongoClient: true}
+  );
+
+  promise.then(function(db) {
+    // Use the stored schema if possible, otherwise create it
+    if(storedModels['access_log']) {
+      var access = storedModels['access_log'];
+    } else {
+     // Usually this is stored in a different folder, but for now put it here
+     var access = storedModels['access_log'] = db.model('access_log', { date: String });
+    }
+
+    var newAccess = new access;
+    newAccess.date = new Date();
+
+    newAccess.save(function(err) {
+      if(err) {
+        res.send("Error saving into database");
+      } else {
+        access.find(function(err, log) {
+          res.render('mongo-db', {title: "MongoDB Log Page", log: log.reverse(), host: mongoHost});
         })
       }
     })
